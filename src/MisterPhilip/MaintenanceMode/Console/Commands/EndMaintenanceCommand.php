3b8f54a0ea02168591f995494768a26fc71becc3
Philip Lawrence <philip@misterphilip.com>
2018-04-01 23:08:47 -0700

    Show total downtime on the up command.

--- src/MisterPhilip/MaintenanceMode/Console/Commands/EndMaintenanceCommand.php
+++ src/MisterPhilip/MaintenanceMode/Console/Commands/EndMaintenanceCommand.php
@@ -3,6 +3,7 @@
 namespace MisterPhilip\MaintenanceMode\Console\Commands;
 
 use Event;
+use Carbon\Carbon;
 use Illuminate\Foundation\Console\UpCommand;
 use MisterPhilip\MaintenanceMode\Events\MaintenanceModeDisabled;
 
@@ -29,14 +30,18 @@ class EndMaintenanceCommand extends UpCommand
 
         // Grab the data
         $data = @json_decode(file_get_contents($this->laravel->storagePath().'/framework/down'), true);
-        if(!isset($data) || is_array($data))
+        if(!isset($data) || !is_array($data))
         {
             $data = [];
         }
         $data = array_merge(["time" => null, "message" => null, "view" => null, "retry" => null], $data);
 
-        // Call the original method, unlinking the down file & console output
-        parent::handle();
+        // Remove the down file
+        @unlink(storage_path('framework/down'));
+
+        // Show how long the application was down for
+        $startingTime = Carbon::createFromTimestamp($data['time']);
+        $this->info("The application is now live! Total downtime: " . Carbon::now()->diffForHumans($startingTime, true, true, 6));
 
         // Fire the event
         Event::fire(new MaintenanceModeDisabled($data['time'], $data['message'], $data['view'], $data['retry']));
